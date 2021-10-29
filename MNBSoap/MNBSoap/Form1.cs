d819085421ec8215b68ee17cb97a892f700360b5
vboti <vboti@DESKTOP-GOLM3F2>
2021-10-29 11:32:06 +0200

    Feladat befejezése

--- MNBSoap/MNBSoap/Form1.cs
+++ MNBSoap/MNBSoap/Form1.cs
@@ -1,34 +1,111 @@
-﻿using MNBSoap.MnbServiceReference;
+﻿using MNBSoap.Entities;
+using MNBSoap.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace MNBSoap
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<string> currencies = new BindingList<string>();
+
         public Form1()
         {
             InitializeComponent();
-            Consume();
+
+            comboBox1.DataSource = currencies;
+
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetCurrenciesRequestBody request = new GetCurrenciesRequestBody();
+            var response = mnbService.GetCurrencies(request);
+            string result = response.GetCurrenciesResult;
+            XmlDocument vxml = new XmlDocument();
+            vxml.LoadXml(result);
+            foreach (XmlElement item in vxml.FirstChild.ChildNodes)
+            {
+                currencies.Add(item.InnerText);
+            }
+   
+            RefreshData();
         }
 
-        void Consume()
+        private void RefreshData()
         {
+            if (comboBox1.SelectedItem == null) return;
+
+            Rates.Clear();
+            string xmlstring = Consume();
+            LoadXml(xmlstring);
+            dataGridView1.DataSource = Rates;
+            Charting();
+        }
+
+        private void Charting()
+        {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+        }
+        private void LoadXml(string input)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(input);
+            foreach (XmlElement item in xml.DocumentElement)
+            {
+                RateData r = new RateData();
+                r.Date = DateTime.Parse(item.GetAttribute("date"));
+                XmlElement child = (XmlElement)item.FirstChild;
+                if (child == null) continue;
+                r.Currency = child.GetAttribute("curr");
+                r.Value = decimal.Parse(child.InnerText);
+                int unit = int.Parse(child.GetAttribute("unit"));
+                if (unit != 0)
+                    r.Value = r.Value / unit;
+
+                Rates.Add(r);
+            }
+
+        }
+
+        string Consume()
+        {
+    
+
             MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
-            request.currencyNames = "EUR";
-            request.startDate = "2020-01-01";
-            request.endDate = "2020-06-30";
+            request.currencyNames = comboBox1.SelectedItem.ToString() ; //"EUR";
+            request.startDate = dateTimePicker1.Value.ToString("yyyy-MM-dd"); //"2020-01-01";
+            request.endDate = dateTimePicker2.Value.ToString("yyyy-MM-dd"); //"2020-06-30";
             var response = mnbService.GetExchangeRates(request);
             string result = response.GetExchangeRatesResult;
+            return result;
 
+
+        }
+
+        private void filterChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
