a86eeda82914d687d62267fbe40b6ab61b3a28fe
Botond Vigh <vbotinho@gmail.com>
2021-11-25 18:42:38 +0100

    email validálás teszt kifejtése

---
+++ Week08_Reye0u/UnitTestExample/UnitTestExample.Test/AccountControllerTestFicture.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    class AccountControllerTestFicture
+    {
+        [Test]
+        private void TestValidateEmail(string email, bool expectedResult)
+        {
+            //Arrange
+            var accountController = new AccountController();
+
+
+
+            //Act
+
+            var valtozo = accountController.ValidateEmail(email);
+
+            //Assert
+            Assert.AreEqual(expectedResult,valtozo);
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Week08_Reye0u/UnitTestExample/UnitTestExample.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1af8163c-6fae-44b3-9853-8a505b02b5f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
